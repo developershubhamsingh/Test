7cce71518337ef184ca34fbce34147768a643f07
developershubhamsingh <shubhamsinghworkid@gmail.com>
2025-09-08 09:30:29 -0700

    .

--- node/fullstack/apps.js
+++ node/fullstack/apps.js
@@ -1,23 +1,38 @@
 let express = require('express');
+const { hello } = require('fund');
 // express is the framework of node js .
 //to access the methods of express .
 // we have to create the object of express.
 let app = express();
 let port = 7000;
 
-let categoryRouter = require('./src/controller/categoryRouter')();
-let productsRouter = require('./src/controller/productsRouter')();
+let navLink = [
+    { link: '/', name: 'Home' },
+    { link: '/category', name: 'Category' },
+    { link: '/products', name: 'Products' }
+]
+
+let categoryRouter = require('./src/controller/categoryRouter')(navLink);
+let productsRouter = require('./src/controller/productsRouter')(navLink);
+
+
+//static files path //
+app.use(express.static(__dirname + '/public'))
+//ejs file path //
+app.set("views", "./src/view");
+//view engine  //
+app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    res.send("Hello from express ! ");
-    res.end();
+    // res.send("Hello from express ! ");
+    res.render('index', { title: "Home Page", text:"hello from nodes"})
 })
 
- 
 
 
-app.use("/category",categoryRouter)
-app.use("/products",productsRouter)
+
+app.use("/category", categoryRouter)
+app.use("/products", productsRouter)
 
 app.listen(port, (err) => {
     if (err) throw err;
